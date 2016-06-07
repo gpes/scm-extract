6629f0efe5fde8a202a4970aebf13ee0af5c9dc9
Ricardo Job <sousajob@gmail.com>
2016-06-07 14:31:35 -0300

    Add checkout in folder

--- src/main/java/com/mycompany/jgit/Loader.java
+++ src/main/java/com/mycompany/jgit/Loader.java
@@ -10,21 +10,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
 /**
  *
  * @author Anderson Souza
  */
 public class Loader {
 
-    public static final File DIRECTORY = new File("C:/Users/Pris/Desktop/Teste");
+    public static final File DIRECTORY = new File("/Users/job/Documents/dev/testes/exemplo-jcabi/dataTeste/v2");
     public static final String REMOTE_URL = "https://github.com/prisgouveia/prisgouveia.github.io";
+    public static final String HASH = "5f465908eb48fa83c9b9aa15db1ced7f7d1d09c2";
     
     public static void main(String[] args) {
         
-//        Git git = Git.cloneRepository().setURI("https://github.com/jass2125/Academia").setDirectory(new File("/home/jairanderson/Área de Trabalho/JGit")).call();
         AbstractFactory factory = new Factory();
         try {
+//            Git repository = factory.createGithub().clone(DIRECTORY, "/Users/job/Documents/dev/testes/exemplo-jcabi/dataTeste/v1");
             Git repository = factory.createGithub().clone(DIRECTORY, REMOTE_URL);
+            Ref call = repository.checkout().setName(HASH).call();
+            
+            
+            
         } catch (GitAPIException ex) {
             Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
         }
